547042f4f981cc84774182a3e9fabbf15e4217c0
Michel <michelmpin@gmail.com>
2021-02-22 17:15:59 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+
+import {ExperienceBar} from './components/ExperienceBar'
+
+function App() {
+  return (
+    <div className="conteiner">
+      <ExperienceBar/>      
+    </div>
+  );
+}
+
+export default App;
